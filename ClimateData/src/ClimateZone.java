fd4ac23cf6e1473d2615ac21d49ed38fc83f50d0
johnst101 <tylercjohnson16@gmail.com>
2023-02-21 09:11:58 -0600

    First submission attempt

--- ClimateData/src/ClimateZone.java
+++ ClimateData/src/ClimateZone.java
@@ -5,51 +5,91 @@ import java.io.IOException;
 
 public class ClimateZone {
 	private ArrayList<City> cityList;
-	
+
 	public ClimateZone() {
 		this.cityList = new ArrayList<City>();
 	}
-	
+
 	public ClimateZone(String fileName) throws IOException {
 		this.cityList = new ArrayList<City>();
-		FileInputStream inFS = new FileInputStream(fileName);
-		Scanner scnr = new Scanner(inFS);
-		
-		String line = inFS.nextLine()
-		String name = 
-		String state = 
-		double highTemp = 
-		double lowTemp = 
+		FileInputStream zoneFile = new FileInputStream(fileName);
+		Scanner inFS = new Scanner(zoneFile);
+
+		while (inFS.hasNext()) {
+			String line = inFS.nextLine();
+			String name = line.substring(0, line.indexOf(" ")); 
+			String state = line.substring(line.indexOf(" ") + 1, line.indexOf(" ", line.indexOf(" ") + 1));
+			double highTemp = Double.parseDouble(line.substring(line.indexOf(" ", line.indexOf(" ") + 1), line.lastIndexOf(" ")));
+			double lowTemp = Double.parseDouble(line.substring(line.lastIndexOf(" "), line.length()));
+			City newCity = new City(name, state, highTemp, lowTemp);
+			this.cityList.add(newCity);
+		}
+
+		inFS.close();
+		zoneFile.close();
 	}
-//	The overloaded constructor should instantiate a new ArrayList object, and store a reference to this object in the ClimateZone's cityList instance variable. It should then open the climate zone data file named in the parameter variable. It should then read in the data from this file and, for each line in the file, it should create (instantiate) a new City object with the name, state, high temp., and low temp. on that line, and add this City to the ArrayList cityList.
+
 	public void addCity(String name, String state, double highTemp, double lowTemp) {
-		
+		City newCity = new City(name, state, highTemp, lowTemp);
+		this.cityList.add(newCity);
 	}
-	
+
 	public int getCityCount() {
 		return this.cityList.size();
 	}
-	
+
 	public City getCityByName(String name, String state) {
-		
+		for (City currentCity : cityList) {
+			if (currentCity.getName().equals(name) && currentCity.getState().equals(state)) {
+				return currentCity;
+			}
+		}
+		return null;
 	}
 
 	public void printHottestCities() {
+		City firstHottest = cityList.get(0);
+		City nextColdest = cityList.get(1);
 		
-	}
-	
-	public void printColdestCities() {
+		for (City currentCity : cityList) {
+			if (currentCity.getHighTemp() > firstHottest.getHighTemp()) {
+				firstHottest = currentCity;
+			}
+		}
 		
-	}
-	
-	public void printAllCities() {
+		for (City currentCity : cityList) {
+			if ((currentCity.getHighTemp() > nextColdest.getHighTemp()) && (currentCity != firstHottest)) {
+				 nextColdest = currentCity;
+			}
+		}
 		
+		firstHottest.printInfo();
+		nextColdest.printInfo();
 	}
 
-//	The addCity method should instantiate a new City object with the name, state, high temp., and low temp. provided in the argument values, and then add this City to the ClimateZone's ArrayList<City> object.
-//	The getCityCount method should return the number of cities currently stored in the ClimateZone's ArrayList<City> object.
-//	The getCityByName method should iterate through the ClimateZone's ArrayList<City> object and search for a City with a name and state that are equal to the argument values. If such a City is found, then this method should return a reference to that City object, otherwise this method should return null.
-//	The printHottestCities method should determine the two Cities objects with the highest high temps. (in descending order). It should then call the printInfo method on these City objects.
-//	The printColdestCities method should determine the two City objects with the lowest low temps. (in ascending order). It should then call the printInfo method on these City objects.
-//	The printAllCities method should iterate through the ClimateZone's ArrayList<City> object and call the printInfo method on each of these City objects.
+	public void printColdestCities() {
+		City firstColdest = cityList.get(0);
+		City nextColdest = cityList.get(1);
+		
+		for (City currentCity : cityList) {
+			if (currentCity.getLowTemp() < firstColdest.getLowTemp()) {
+				firstColdest = currentCity;
+			}
+		}
+		
+		for (City currentCity : cityList) {
+			if ((currentCity.getLowTemp() < nextColdest.getLowTemp()) && (currentCity != firstColdest)) {
+				 nextColdest = currentCity;
+			}
+		}
+		
+		firstColdest.printInfo();
+		nextColdest.printInfo();
+	}
+
+	public void printAllCities() {
+		for (City currentCity : cityList) {
+			currentCity.printInfo();
+		}
+	}
 }
